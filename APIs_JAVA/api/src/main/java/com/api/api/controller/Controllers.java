66d494dba0c158a2473aa22b2e3e1da1c09a0344
jorge <jorgeabp99@gmail.com>
2025-05-01 10:36:40 -0300

    pc jorge

--- APIs_JAVA/api/src/main/java/com/api/api/controller/Controllers.java
+++ APIs_JAVA/api/src/main/java/com/api/api/controller/Controllers.java
@@ -3,9 +3,14 @@ package com.api.api.controller;
 import com.api.api.Dto.RegistroDto;
 import com.api.api.models.Users;
 import com.api.api.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/getUsers")
 public class Controllers {
 
 }
