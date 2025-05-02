8ba80eb19ee0b5f58b04c3ef3ad9853827c85274
jorge.prado <jorge@hs-software.dev.br>
2025-05-02 08:08:35 -0300

    pc hs

--- APIs_JAVA/api/src/main/java/com/api/api/controller/authorization/AuthorizationFilter.java
+++ APIs_JAVA/api/src/main/java/com/api/api/controller/authorization/AuthorizationFilter.java
@@ -3,8 +3,12 @@ package com.api.api.controller.authorization;
 
 import com.api.api.services.authorization.AuthorizationService;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,7 +24,29 @@ public class AuthorizationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws  SecurityException, IOException {
-        
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws SecurityException, IOException, ServletException {
+
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        if (authorizationService.isToken(token)) {
+            String username = authorizationService.getUsernameFromToken(token);
+
+            // Aqui você pode criar uma autenticação básica
+            UsernamePasswordAuthenticationTokennToken auth =
+                    new UsernamePasswordAuthenticationToken(username, null, null);
+            auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
     }
 }
--- APIs_JAVA/api/src/main/java/com/api/api/services/authorization/AuthorizationService.java
+++ APIs_JAVA/api/src/main/java/com/api/api/services/authorization/AuthorizationService.java
@@ -1,4 +1,36 @@
 package com.api.api.services.authorization;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthorizationService {
+
+    @Value("${jwt.secret}")
+    private String secreKey;
+
+    public boolean isToken(String token) {
+        Claims claims = parseToken(token);
+        if (claims.getSubject() != null && !claims.getSubject().isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    private Claims parseToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secreKey.getBytes())
+                .parseClaimsJws(token.replace("Bearer ", ""))
+                .getBody();
+    }
+
+    public String getUsernameFromToken(String token){
+        Claims claims = parseToken(token);
+        return claims.getSubject();
+    }
 }
