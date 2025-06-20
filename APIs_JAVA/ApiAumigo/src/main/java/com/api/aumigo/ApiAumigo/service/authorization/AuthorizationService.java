feea664a4522f8b5b179418cf85354aa1d8df632
jorge.prado <jorge@hs-software.dev.br>
2025-06-20 10:54:39 -0300

    pc hs

--- APIs_JAVA/ApiAumigo/src/main/java/com/api/aumigo/ApiAumigo/service/authorization/AuthorizationService.java
+++ APIs_JAVA/ApiAumigo/src/main/java/com/api/aumigo/ApiAumigo/service/authorization/AuthorizationService.java
@@ -46,12 +46,8 @@ public class AuthorizationService {
             return true;
 
         } catch (Exception e) {
-<<<<<<< HEAD
             e.printStackTrace();
             return false;
-=======
-            return false; //capturar a exception
->>>>>>> fbdedf1350f0c9c83374f9da4f92c75a72465ba2
         }
     }
 
